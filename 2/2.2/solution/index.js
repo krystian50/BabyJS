bea3a66f9e165908e74588f6643e07530791a27a
Krystian Fras <krystian@fras.me>
2021-01-12 23:53:53 +0100

    Add Szymon's solution for ex2.2

--- 2/2.2/solution/index.js
+++ 2/2.2/solution/index.js
@@ -1,18 +1,31 @@
-const httpService = {
-  get: (url) => fetch(`https://swapi.dev/api${url}`).then((response) => {
-    if (response.status === 200) {
-      return response.json();
-    }
+const baseUrl = 'https://swapi.dev/api';
+const peoplePath = '/people';
 
-    throw new Error(response.statusText);
-  })
-};
+const searchHandler = (ev) => {
+    ev.preventDefault();
+    const searchValue = document.querySelector('#searchInput').value;
 
-httpService.get('/people/?search=r2').then(({ results }) => {
-  const person = results[0]
-  const { name, gender, birth_year } = person
+    search(searchValue);
+}
 
-  document.querySelector('#name').textContent = name
-  document.querySelector('#gender').textContent = gender
-  document.querySelector('#birth_year').textContent = birth_year
-})
+const HttpClient = {
+    get: url => fetch(`${baseUrl}${url}`)
+        .then(resp => resp.json())
+}
+
+const updateTextNode = (selector, value) => { document.querySelector(selector).textContent = value; }
+
+const search = (searchValue) => {
+    HttpClient.get(`${peoplePath}?search=${searchValue}`)
+        .then(val => {
+            if (val.results.length === 0) {
+                return
+            }
+
+            const { name, gender, birth_year } = val.results[0]
+
+            updateTextNode('#name', name);
+            updateTextNode('#gender', gender);
+            updateTextNode('#birthYear', birth_year);
+        })
+}
